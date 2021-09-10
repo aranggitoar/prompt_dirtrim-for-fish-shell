dcbe0514f54e9cc84da05bab1ae2fac1cd6dc65d
aranggitoar <aranggi.josef@gmail.com>
2021-09-10 12:30:57 +0700

    Added short license notice on each file

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+# PROMPT_DIRTRIM for Fish Shell
+# A fish shell function that acts like the PROMPT_DIRTRIM variable of bash.
+# Copyright (C)	2021-present	Aranggi Toar
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Full notice: https://github.com/aranggitoar/prompt_dirtrim-for-fish-shell/blob/main/LICENSE
+# Or ./LICENSE in the original source tree.
+
 #!/bin/bash
 
 read -p "This script will copy the prompt_dirtrim function for fish shell and open your config.fish for you. Ready? (y/n) " CONFIRMATION
--- prompt_dirtrim.fish
+++ prompt_dirtrim.fish
@@ -1,3 +1,15 @@
+# PROMPT_DIRTRIM for Fish Shell
+# A fish shell function that acts like the PROMPT_DIRTRIM variable of bash.
+# Copyright (C)	2021-present	Aranggi Toar
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# Full notice: https://github.com/aranggitoar/prompt_dirtrim-for-fish-shell/blob/main/LICENSE
+# Or ./LICENSE in the original source tree.
+
 function prompt_dirtrim -d "Trim the prompt_pwd length into the specified count"
 
 	set -l pwd_length (math (prompt_pwd | string split / | count) - 1)
